e0dcfcdea0e1d62661ebdd00ec93bf0ad9e2905e
Edilton X <ediltonx@edilton.com>
2025-10-20 10:08:31 -0300

    fix: corrigir import de CSS em FaqSection - alterar FAQSection.css para FaqSection.css

--- frontend/src/components/FaqSection/FaqSection.jsx
+++ frontend/src/components/FaqSection/FaqSection.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaw, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
-import './FAQSection.css'
+import './FaqSection.css'
 
 const faqItems = [
   {
